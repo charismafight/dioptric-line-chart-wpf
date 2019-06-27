ae56ec40704faea6d7d956d6f0500099927460e3
Lee <charismafight@hotmail.com>
2019-06-27 23:51:48 +0800

    init

---
+++ Dioptric/Dioptric/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dioptric
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        public int Age { get; set; }
+
+        public string IDCardNumber { get; set; }
+
+        public float EyeSight { get; set; }
+    }
+}
